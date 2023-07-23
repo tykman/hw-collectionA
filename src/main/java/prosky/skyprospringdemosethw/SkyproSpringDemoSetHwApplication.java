035874bd90909c8b6e286000afaadf5a6a7a51de
tykman <nizom.ra@inbox.ru>
2023-07-23 11:52:51 +0500

    Мой первый коммит.

---
+++ src/main/java/prosky/skyprospringdemosethw/SkyproSpringDemoSetHwApplication.java
@@ -0,0 +1,13 @@
+package prosky.skyprospringdemosethw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringDemoSetHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringDemoSetHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prosky/skyprospringdemosethw/SkyproSpringDemoSetHwApplicationTests.java
@@ -0,0 +1,13 @@
+package prosky.skyprospringdemosethw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringDemoSetHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
